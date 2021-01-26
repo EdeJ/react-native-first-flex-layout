288d347309599a3dd501796cf94d01f1c719293f
Emiel de Jong <emieldejong@xs4all.nl>
2021-01-26 18:02:14 +0100

    update

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'react-native';
 import React from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={{ height: 70, backgroundColor: 'darkred' }} />
+      <StatusBar barStyle='light-content' backgroundColor="darkred" />
+      <View style={{ height: 40, backgroundColor: 'darkred' }} />
       <View style={{ flex: 2, flexDirection: 'row' }}>
-        <View style={{ paddingTop: 20, paddingBottom: 20, justifyContent: 'space-between', alignItems: 'center', height: '100%', width: '50%', backgroundColor: 'lightpink' }} >
+        <View style={{ paddingTop: 20, paddingBottom: 20, justifyContent: 'space-between', alignItems: 'center', height: '100%', flex: 1, backgroundColor: 'lightpink' }} >
           <Text style={{ color: '#fff' }}>I'am on the left</Text>
           <View style={{ backgroundColor: 'darkred' }}>
-            <Text style={{ color: '#fff', padding: 20 }}>Floatin'here</Text>
+            <Text style={{ color: '#fff', padding: 20 }}>Floatin' here</Text>
           </View>
         </View>
-        <View style={{ alignItems: 'center', justifyContent: 'center', height: '100%', width: '50%', backgroundColor: 'powderblue' }} >
+        <View style={{ alignItems: 'center', justifyContent: 'center', height: '100%', flex: 1, backgroundColor: 'powderblue' }} >
           <Text>I'am on the right</Text>
         </View>
       </View>
-      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', height: 70, backgroundColor: '#fff' }} >
+      <View style={{ padding: 20, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', height: 70, backgroundColor: '#fff' }} >
         <Text style={{ color: 'black' }}>I want all the space</Text>
-        {/* <Text style={{ color: 'black' }}>Button</Text> */}
         <TouchableOpacity style={{}}>
-          <Text>OK</Text>
+          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>OK</Text>
         </TouchableOpacity>
       </View>
     </View >
